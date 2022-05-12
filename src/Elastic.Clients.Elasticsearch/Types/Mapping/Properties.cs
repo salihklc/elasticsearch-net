2c34e2efee646057f37f250d0519d4a6e54ff8ef
Steve Gordon <sgordon@hotmail.co.uk>
2022-05-12 10:24:28 +0100

    Prototype for properties

--- src/Elastic.Clients.Elasticsearch/Types/Mapping/Properties.cs
+++ src/Elastic.Clients.Elasticsearch/Types/Mapping/Properties.cs
@@ -2,10 +2,15 @@
 // Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
 // See the LICENSE file in the project root for more information.
 
+using System;
+using System.Linq.Expressions;
+
 namespace Elastic.Clients.Elasticsearch.Mapping;
 
 public partial class Properties
 {
+	public void Add<T>(Expression<Func<T, object>> name, IProperty property) => BackingDictionary.Add(name, property);
+
 	public bool TryGetProperty<T>(PropertyName propertyName, out T property) where T : PropertyBase
 	{
 		if (BackingDictionary.TryGetValue(propertyName, out var propertyBase) && propertyBase is T finalProperty)
@@ -17,6 +22,21 @@ public partial class Properties
 		property = default;
 		return false;
 	}
+}
+
+public partial class Properties<T> : Properties
+{
+	public void Add<TValue>(Expression<Func<T, TValue>> name, IProperty property) => BackingDictionary.Add(name, property);
+}
+
+public partial class PropertiesDescriptor<T>
+		: IsADictionaryDescriptorBase<PropertiesDescriptor<T>, Properties, PropertyName, IProperty>
+{
+	public PropertiesDescriptor() : base(new Properties<T>()) { }
+
+	public PropertiesDescriptor(Properties properties) : base(properties ?? new Properties<T>()) { }
+
+	//public PropertiesDescriptor<T> Boolean(Func<BooleanPropertyDescriptor<T>, BooleanProperty> selector) => SetProperty(selector);
 }
 
 public partial class TypeMappingDescriptor
